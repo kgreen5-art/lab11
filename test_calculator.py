1b3da050c4c328b216c3db17f7f54db3d9c8e6f9
Kai W <kaiwest@Kais-MacBook-Air.local>
2026-04-08 11:13:03 -0400

    merged changes

--- test_calculator.py
+++ test_calculator.py
@@ -3,12 +3,16 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+     def test_add(self): # 3 assertions
+        self.assertEqual(add(2,4), 6)
+        self.assertEqual(add(-3, 8), 5)
+        self.assertEqual(add(-6, -7), -13)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+     def test_subtract(self): # 3 assertions
+        self.assertEqual(sub(4,2), 2)
+        self.assertEqual(sub(2, 8), -6)
+        self.assertEqual(sub(-4, 7), -11)
+        self.assertEqual(sub(-6, -7), 1)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -19,19 +23,19 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+     def test_divide_by_zero(self): # 1 assertion
+           self.assertRaises(ZeroDivisionError, div(3, 0))
+           self.assertRaises(ZeroDivisionError, div(10, 0))
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+     def test_logarithm(self): # 3 assertions
+        self.assertEqual(log(2, 4), 2)
+        self.assertEqual(log(3, 27), 3)
+        self.assertEqual(log(5, 625), 4)
+
+     def test_log_invalid_base(self): # 1 assertion
+        self.assertRaises(ValueError, log(-3, 9))
+        self.assertRaises(ValueError, log(0, 9))
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
