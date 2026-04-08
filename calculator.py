1d0c2f50d2f862c4b344cf5384408d46cd1d5db3
kgreen5-art <kgreen5@ufl.edu>
2026-04-08 12:30:09 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,41 @@
 #https://github.com/kgreen5-art/lab11
-#Partner 1: Kaesi Green
-# Partner 2: Kai West
+'''
+calculator.py
+- Defines functions used to create a simple calculator
+
+One function per operation, in order.
+'''
 import math
-def square_root(a):
-    if a < 0:
+
+def square_root(a): # raise ValueError if a < 0
+    if a<0:
         raise ValueError
     return math.sqrt(a)
-def hypotenuse(a, b):
+
+def hypotenuse(a, b):  # can have negative nums
     return math.hypot(a, b)
 
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def logarithm(a, b):
+    return math.log(b, a)# use math library/raise ValueError
+
+def exponent(a, b):
+    return a**b
+
+# p2
 def add(a, b):
     return a +b
 def sub(a,b):
@@ -16,10 +43,10 @@ def sub(a,b):
 def mul(a,b):
     return a * b
 def div(a, b):
-    if a == 0:
+    if b == 0:
         raise ZeroDivisionError
-    return b/a
-def logarithm(a,b):
+    return a/b
+def log(a,b):
     if a<= 0:
         raise ValueError
     if b<= 0:
@@ -27,8 +54,3 @@ def logarithm(a,b):
     return math.log(b, a)
 def exp(a,b):
     return a**b
-
-
-
-
-
